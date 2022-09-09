8f9908b96dae4068c071f8055be145fd77ddd0cf
iris-correa <iris555.correa@yahoo.com>
2022-09-09 14:51:44 -0300

    dolar and euro converter function

--- money_converter/src/app/home/home.page.ts
+++ money_converter/src/app/home/home.page.ts
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  
+  dolar_value;
+  euro_value;
+  dolar = 5.21;
+
+
+  dolar_converter(formConverter){
+    let real = formConverter.value.real_value;
+
+    let dolar = 5.21;
+    
+    this.dolar_value = dolar*parseFloat(real)
+
+  }
+
+  euro_converter(formConverter){
+    let real = formConverter.value.real_value;
+
+    let euro = 5.22;
+    
+    this.euro_value = euro*parseFloat(real)
+  }
 
 }
